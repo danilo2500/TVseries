fac6e1f554715b59308be1b584c21966cb75621f
Danilo Henrique <danilo.2500@gmail.com>
2022-04-15 11:09:43 -0300

    Remove storyboard

--- TVSeries/TVSeries/SeriesViewController.swift
+++ TVSeries/TVSeries/SeriesViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SeriesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
